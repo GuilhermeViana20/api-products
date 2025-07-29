5bdab9d0e1be9ce71b1f0bff68dead51aa3d9b02
Guilherme Viana <guilhermeviana347@gmail.com>
2025-07-29 17:34:15 -0300

    Correção do endereço novo da api

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const setupRoutes = require('./routes');
 const cors = require('cors');
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://api-products-theta.vercel.app'] }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://app-scanner.netlify.app'] }));
 app.use(express.json());
 
 (async () => {
