bc9f0ad0dcd7ea639d581f05cda631c5958904d0
Guilherme Viana <guilhermeviana347@gmail.com>
2025-07-29 17:14:46 -0300

    Ajuste para deploy funcionar

--- src/server.js
+++ src/server.js
@@ -10,11 +10,8 @@ app.use(express.json());
 (async () => {
   const { sheets, spreadsheetId } = await setupSheets();
   const routes = await setupRoutes({ sheets, spreadsheetId });
-  const PORT = process.env.PORT || 3000;
 
   app.use('/api', routes);
-
-  app.listen(PORT, () => {
-    console.log(`API rodando em http://localhost:${PORT}/api`);
-  });
 })();
+
+module.exports = app;
